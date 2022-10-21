17a1ead2166cebc1b9e5d292597eb0f32e31bd19
Adenilson Gonçalves <dennysguytar@hotmail.com>
2022-10-21 17:41:37 -0300

    Ajustes realizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(99) 99999-9999", modelo: "Redmi Note 8", imei: "54645546464652", memoria: 128);
+Console.WriteLine($"Número: {nokia.Numero}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "(88) 88888-8888", modelo: "Redmi Note 9S", imei: "6546546545466", memoria: 128);
+Console.WriteLine($"Número: {iphone.Numero}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
